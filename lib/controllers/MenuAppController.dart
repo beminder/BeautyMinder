7a775aab826d928275d509c9622f7f93d32b1c79
LeeZEun <je990420@ajou.ac.kr>
2023-12-10 22:37:47 +0900

    Update: Profile Page

--- lib/controllers/MenuAppController.dart
+++ lib/controllers/MenuAppController.dart
@@ -5,7 +5,8 @@ class MenuAppController extends ChangeNotifier {
 
   GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKey;
 
-  String _selectedScreen = 'dashboard';
+  // String _selectedScreen = 'dashboard';
+  String _selectedScreen = 'profile';
 
   String get selectedScreen => _selectedScreen;
 
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,9 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 import '../../Service/api_service.dart';
 import '../../constants.dart';
 import '../../dto/user_model.dart';
 import '../dashboard/components/header.dart';
+import '../start/splash_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
 
@@ -39,61 +41,188 @@ class ProfileScreen extends StatelessWidget {
                 future: APIService.getUserProfile(),
                 builder: (context, snapshot) {
                   final userProfileResult = snapshot.data?.value;
-                  return Container(
-                    width: MediaQuery.of(context).size.width/2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('아이디'),
-                            Spacer(),
-                            Text('${userProfileResult?.id}' ?? '정보 없음')
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('이메일'),
-                            Spacer(),
-                            Text('${userProfileResult?.email}' ?? '정보 없음')
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('닉네임'),
-                            Spacer(),
-                            Text('${userProfileResult?.nickname}' ?? '정보 없음')
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('전화번호'),
-                            Spacer(),
-                            Text('${userProfileResult?.phoneNumber}' ?? '정보 없음')
-                          ],
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('생성 시각'),
-                            Spacer(),
-                            Text('${userProfileResult?.createdAt}' ?? '정보 없음')
-                          ],
-                        )
-                      ],
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height/1.5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(
+                                userProfileResult?.profileImage ?? ''),
+                          ),
+                          SizedBox(height: 30,),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '아이디',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${userProfileResult?.id}' ?? '정보 없음',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Divider(
+                            color: Colors.white,
+                            thickness: 0.5,
+                            endIndent: 5,
+                          ),
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '이메일',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${userProfileResult?.email}' ?? '정보 없음',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Divider(
+                            color: Colors.white,
+                            thickness: 0.5,
+                            endIndent: 5,
+                          ),
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '닉네임',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${userProfileResult?.nickname}' ?? '정보 없음',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Divider(
+                            color: Colors.white,
+                            thickness: 0.5,
+                            endIndent: 5,
+                          ),
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '전화번호',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${userProfileResult?.phoneNumber}' ?? '정보 없음',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Divider(
+                            color: Colors.white,
+                            thickness: 0.5,
+                            endIndent: 5,
+                          ),
+                          Spacer(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '생성 시각',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${userProfileResult?.createdAt}' ?? '정보 없음',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Divider(
+                            color: Colors.white,
+                            thickness: 0.5,
+                            endIndent: 5,
+                          ),
+                          Spacer(),
+                        ],
+                      ),
                     ),
                   );
                 },
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: InkWell(
+                  onTap: () async {
+                    try {
+                      final logoutResponse = await APIService.logout();
+                      if(logoutResponse.isSuccess) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('로그아웃에 성공하였습니다.'),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => SplashScreen()),
+                        );
+                      }
+                      else {
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('로그아웃에 실패하였습니다.'),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                      }
+                    }
+                    catch (e) {
+                      Navigator.pop(context);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('오류가 발생했습니다. 잠시 후 다시 시도해주세요.'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 13),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: secondaryColor),
+                    child: Text(
+                      '로그아웃',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
